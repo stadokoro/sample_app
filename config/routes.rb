ab0b366822c6614a7da99d8ac854ae7ed2eaa852
jordi1961 <satorumaro@gmail.com>
2015-08-23 02:56:06 +0000

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ SampleApp::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "registrations"
   }
-
-  get "users/show"
   resources :users, only: [:show]
   root 'static_pages#home'
+  #get "users/show"
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
